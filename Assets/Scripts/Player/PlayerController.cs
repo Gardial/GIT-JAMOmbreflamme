b8ef7657b13fe2bb491815564fe05669a226d632
Sebastien_Verwerft <sebastien.verwerft@gmail.com>
2021-11-07 16:20:59 +0100

    Add sprites animation + wip player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Settings")]
-    [Range(1, 5)]
+    [Range(1, 10)]
     public float RangeAtk;
     public int health;
+    public int damage;
     [Range(1, 5)]
     public float invincibleTime;
 
@@ -18,6 +19,12 @@ public class PlayerController : MonoBehaviour
     private List<GameObject> lstForwardEnemies; // Enemies forward
 
     private bool isInvincible;
+
+    private Vector2 targetPosition;
+    private GameObject target;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +37,9 @@ public class PlayerController : MonoBehaviour
 
         transform.GetChild(1).GetComponent<BoxCollider2D>().size = new Vector2(RangeAtk, 3);
         transform.GetChild(1).GetComponent<BoxCollider2D>().offset = new Vector2(RangeAtk/2, -1);
+
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -39,15 +49,19 @@ public class PlayerController : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow) && lstBehindEnemies.Count > 0)
             {
-                transform.position = new Vector2(lstBehindEnemies[0].transform.position.x, transform.position.y);
-                Destroy(lstBehindEnemies[0]);
+                targetPosition = new Vector2(lstBehindEnemies[0].transform.position.x, transform.position.y);
+                target = lstBehindEnemies[0];
+                spriteRenderer.flipX = true;
+                animator.SetTrigger("Attack");
                 lstBehindEnemies.Remove(lstBehindEnemies[0]);
             }
 
             if (Input.GetKeyDown(KeyCode.RightArrow) && lstForwardEnemies.Count > 0)
             {
-                transform.position = new Vector2(lstForwardEnemies[0].transform.position.x, transform.position.y);
-                Destroy(lstForwardEnemies[0]);
+                targetPosition = new Vector2(lstForwardEnemies[0].transform.position.x, transform.position.y);
+                target = lstForwardEnemies[0];
+                spriteRenderer.flipX = false;
+                animator.SetTrigger("Attack");
                 lstForwardEnemies.Remove(lstForwardEnemies[0]);
             }
         }
@@ -55,6 +69,16 @@ public class PlayerController : MonoBehaviour
         else
         {
             gameOver.SetActive(true);
+        }
+
+        if(lstBehindEnemies.Count > 0 && lstBehindEnemies[0] == null)
+        {
+            lstBehindEnemies.RemoveAt(0);
+        }
+
+        if (lstForwardEnemies.Count > 0 && lstForwardEnemies[0] == null)
+        {
+            lstForwardEnemies.RemoveAt(0);
         }
     }
 
@@ -64,7 +88,7 @@ public class PlayerController : MonoBehaviour
         {
             health -= collision.gameObject.GetComponent<Enemy>().damage;
             isInvincible = true;
-            StartCoroutine(BecomeTemporarilyInvincible());
+            //StartCoroutine(BecomeTemporarilyInvincible());
         }
     }
 
@@ -89,5 +113,18 @@ public class PlayerController : MonoBehaviour
         material.color = Vector4.zero;
         yield return new WaitForSeconds(invincibleTime);
         isInvincible = false;
+    }
+
+    public void TeleportPlayer()
+    {
+        transform.position = targetPosition;
+    }
+
+    public void DealDamage()
+    {
+        if(target.GetComponent<Enemy>() != null)
+        {
+            target.GetComponent<Enemy>().health -= damage;
+        }
     }
 }
