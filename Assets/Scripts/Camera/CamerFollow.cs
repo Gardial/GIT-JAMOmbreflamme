4a499193d2b98039485519934018718ac6e80116
Sebastien_Verwerft <sebastien.verwerft@gmail.com>
2021-11-07 21:10:45 +0100

    FInal scene update

--- Assets/Scripts/Camera/CamerFollow.cs
+++ Assets/Scripts/Camera/CamerFollow.cs
@@ -13,31 +13,26 @@ public class CamerFollow : MonoBehaviour
 
     private void Start()
     {
-        startpos = transform.position.x;
-        if (!GameObject.Find("Main Camera"))
-        {
-            length = GetComponent<SpriteRenderer>().bounds.size.x;
-        }
+        //startpos = transform.position.x;
+        //if (!GameObject.Find("Main Camera"))
+        //{
+        //    length = GetComponent<SpriteRenderer>().bounds.size.x;
+        //}
     }
 
     private void Update()
     {
-        float temp = (transform.position.x * (1 - parallaxEffect));
-        float dist = (transform.position.x * parallaxEffect);
+        //float temp = (transform.position.x * (1 - parallaxEffect));
+        //float dist = (transform.position.x * parallaxEffect);
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+        //if (temp > startpos + length) startpos += length;
+        //else if (temp < startpos - length) startpos -= length;
     }
 
     void LateUpdate()
     {
-    /*    float temp = (transform.position.x * (1 - parallaxEffect));
-        float dist = (transform.position.x * parallaxEffect); */
 
         float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smooth);
         transform.position = new Vector3(posX, transform.position.y, transform.position.z);
-
-    /*    if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length; */
     }
 }
